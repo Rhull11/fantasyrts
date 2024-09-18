9a985e845c8a413758f51fe40aa25c0f720aba8a
Rhull11 <rhull9@outlook.com>
2024-09-18 01:27:23 -0600

    Added Animation Tree and got directional movement with idle for now.

--- Level1.cs
+++ Level1.cs
@@ -5,20 +5,19 @@ public partial class Level1 : Node2D
 {
 
 	private CharacterBody2D _orcBody2D;
-	private AnimatedSprite2D _orcAnimatedSprite2D;
-	private AnimatedSprite2D _shadowAnimatedSprite2D;
+
+	private AnimationPlayer _animationPlayer;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_orcBody2D = GetNode<CharacterBody2D>("OrcBody2D");
-		_orcAnimatedSprite2D = _orcBody2D.GetChild<AnimatedSprite2D>(0);
-		_shadowAnimatedSprite2D = _orcBody2D.GetChild<AnimatedSprite2D>(1);
+		_animationPlayer = _orcBody2D.GetChild<AnimationPlayer>(3);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		_orcAnimatedSprite2D.Play("idle_armed");
-		_shadowAnimatedSprite2D.Play("idle_shadow_armed");
+		_animationPlayer.Play("idle_down");
 	}
 }
--- OrcBody2D.cs
+++ OrcBody2D.cs
@@ -6,26 +6,44 @@ public partial class OrcBody2D : CharacterBody2D
 	[Export]
     public int Speed { get; set; } = 400;
 
-    private Vector2 clickDirection;
-    private Vector2 targetDirection;
+    [Export]
+    public AnimationTree AnimationTree {get; set;}
+
+    private Vector2 _clickDirection;
+    private Vector2 _targetDirection;
+    
+    private AnimationPlayer _animationPlayer;
+    private Sprite2D _orcUp;
+    private Sprite2D _shadowUp;
+
+    private Sprite2D _orcDown;
+    private Sprite2D _shadowDown;
 
     public override void _Ready()
     {        
-        clickDirection = Position;
+        _clickDirection = Position;
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        AnimationTree = GetNode<AnimationTree>("AnimationTree");
+        _orcUp = GetNode<Sprite2D>("OrcSpriteUp");
+        _shadowUp = GetNode<Sprite2D>("ShadowSpriteUp");
+        _orcDown = GetNode<Sprite2D>("OrcSprite");
+        _shadowDown = GetNode<Sprite2D>("ShadowSprite");
     }
 
-    public override void _PhysicsProcess(double delta)
+    public override void _Process(double delta)
     {
+         //_animationPlayer.Play("idle_down");
         
         if(Input.IsActionJustPressed("left_click")){
-            clickDirection = GetGlobalMousePosition();
+            _clickDirection = GetGlobalMousePosition();
         }
         
-        if(Position.DistanceTo(clickDirection) > 3){
-            targetDirection = (clickDirection - Position).Normalized();
-            Velocity = targetDirection.Normalized() * Speed;
+        if(Position.DistanceTo(_clickDirection) > 3){
+            _targetDirection = (_clickDirection - Position).Normalized();
+            Velocity = _targetDirection.Normalized() * Speed;
             MoveAndSlide();
         }
 
+        AnimationTree.Set("parameters/idle/blend_position", Velocity);
     }
 }
